87dbc9034585847499935b3aa582dbd0ea4c5b01
UIRRRRRRRRRRRRRR <96994124+UIRRRRRRRRRRRRRR@users.noreply.github.com>
2022-01-02 21:30:55 +0300

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,140 @@
+import time
+import colorama
+import os
+from termcolor import colored
+from colorama import Fore, Style
+clear = lambda: os.system ("clear")
+clear()
+u = int(input('Введите секретный код, если вы, не хотите вводить код просто нажмите 1 и enter.'))
+if u==443556:
+	clear()
+	print(Fore.RED + 'https://discord.gg/5PBxxfXc9R')
+	time.sleep(0.5)
+	clear()
+	print(Fore.WHITE + 'https://discord.gg/5PBxxfXc9R')
+	time.sleep(9)
+if u==678996:
+	clear()
+	print(Fore.RED + Style.BRIGHT + '                  Программа "bloody calc" 2022')
+	print(Fore.CYAN + Style.NORMAL + 'Программа "bloody calc" — это моя вторая программа на python. Данная программа имеет несколько функций.')
+	print('версия 2022.1.0_android. Для продолжения нажмите enter.')
+input()
+if u==202210:
+	clear()
+	print(Fore.CYAN + 'v2022.1.0.android')
+	print('===============================================================')
+	os.system('cat /proc/cpuinfo')
+	print('===============================================================')
+	os.system('cat /proc/meminfo')
+	print('===============================================================')
+	print('Created by UIRRRRRRRRRRRRRR')
+	print(' ')
+	print(Fore.RED + """
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣧⠻⣿⣿⠿⠿⠿⢿⣿⠟⣼⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⠟⠃⠁⠄⠄⠄⠄⠄⠄⠘⠻⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠃⠄⣴⡄⠄⠄⠄⠄⠄⣴⡆⠄⠘⢿⣿⣿⣿⣿
+⣿⣿⣿⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⣿⣿
+⣿⠿⢿⣿⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⣿⡿⠿⣿
+⡇⠄⠄⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠄⠄⢸
+⡇⠄⠄⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠄⠄⢸
+⡇⠄⠄⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠄⠄⢸
+⡇⠄⠄⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠄⠄⢸
+⣧⣤⣤⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣤⣤⣼
+⣿⣿⣿⣿⣶⣤⡄⠄⠄⠄⣤⣤⣤⠄⠄⠄⢠⣤⣴⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⡇⠄⠄⠄⣿⣿⣿⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣤⣤⣴⣿⣿⣿⣦⣤⣤⣾⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿    
+          ANDROID""")
+input()
+if u==202211:
+	print(Fore.CYAN)
+	clear()
+	os.system('uname')
+	os.system('uname -n')
+	os.system('uname -v')
+	os.system('uname -r')
+	os.system('uname -m')
+	os.system('uname -a')
+	print(Fore.RED)
+	print(""".__  .__                     
+|  | |__| ____  __ _____  ___
+|  | |  |/    \|  |  \  \/  /
+|  |_|  |   |  \  |  />    < 
+|____/__|___|  /____//__/\_\
+                          
+             """)
+input()
+print(Fore.RED)
+clear()
+print(' ▄████▄   ▄▄▄       ██▓     ▄████▄  ')
+time.sleep(0.05)
+print('▒██▀ ▀█  ▒████▄    ▓██▒    ▒██▀ ▀█  ')
+time.sleep(0.05)
+print('▒▓█    ▄ ▒██  ▀█▄  ▒██░    ▒▓█    ▄ ')
+print('▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██░    ▒▓▓▄ ▄██▒')
+print('▒ ▓███▀ ░ ▓█   ▓██▒░██████▒▒ ▓███▀ ░')
+print('░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ▒░▓  ░░ ░▒ ▒  ░')
+print('  ░  ▒     ▒   ▒▒ ░░ ░ ▒  ░  ░  ▒   ')
+print('░          ░   ▒     ░ ░   ░        ')
+print('░ ░            ░  ░    ░  ░░ ░      ')
+print('░                          ░        ')
+print('Выберите первое число')
+x = int(input())
+clear()
+print(' ▄████▄   ▄▄▄       ██▓     ▄████▄  ')
+time.sleep(0.05)
+print('▒██▀ ▀█  ▒████▄    ▓██▒    ▒██▀ ▀█  ')
+time.sleep(0.05)
+print('▒▓█    ▄ ▒██  ▀█▄  ▒██░    ▒▓█    ▄ ')
+print('▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██░    ▒▓▓▄ ▄██▒')
+print('▒ ▓███▀ ░ ▓█   ▓██▒░██████▒▒ ▓███▀ ░')
+print('░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ▒░▓  ░░ ░▒ ▒  ░')
+print('  ░  ▒     ▒   ▒▒ ░░ ░ ▒  ░  ░  ▒   ')
+print('░          ░   ▒     ░ ░   ░        ')
+print('░ ░            ░  ░    ░  ░░ ░      ')
+print('░                          ░        ')
+print('Выберите второе число')
+t = int(input())
+clear()
+print(' ▄████▄   ▄▄▄       ██▓     ▄████▄  ')
+time.sleep(0.05)
+print('▒██▀ ▀█  ▒████▄    ▓██▒    ▒██▀ ▀█  ')
+time.sleep(0.05)
+print('▒▓█    ▄ ▒██  ▀█▄  ▒██░    ▒▓█    ▄ ')
+print('▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██░    ▒▓▓▄ ▄██▒')
+print('▒ ▓███▀ ░ ▓█   ▓██▒░██████▒▒ ▓███▀ ░')
+print('░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ▒░▓  ░░ ░▒ ▒  ░')
+print('  ░  ▒     ▒   ▒▒ ░░ ░ ▒  ░  ░  ▒   ')
+print('░          ░   ▒     ░ ░   ░        ')
+print('░ ░            ░  ░    ░  ░░ ░      ')
+print('░                          ░        ')
+print('Выберите действие')
+print('1.Умножение')
+print('2.Сложение')
+print('3.Деление')
+print('4.Вычитание')
+y = int(input())
+clear()
+print(' ▄████▄   ▄▄▄       ██▓     ▄████▄  ')
+time.sleep(0.05)
+print('▒██▀ ▀█  ▒████▄    ▓██▒    ▒██▀ ▀█  ')
+time.sleep(0.05)
+print('▒▓█    ▄ ▒██  ▀█▄  ▒██░    ▒▓█    ▄ ')
+print('▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██░    ▒▓▓▄ ▄██▒')
+print('▒ ▓███▀ ░ ▓█   ▓██▒░██████▒▒ ▓███▀ ░')
+print('░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ▒░▓  ░░ ░▒ ▒  ░')
+print('  ░  ▒     ▒   ▒▒ ░░ ░ ▒  ░  ░  ▒   ')
+print('░          ░   ▒     ░ ░   ░        ')
+print('░ ░            ░  ░    ░  ░░ ░      ')
+print('░                          ░        ')
+if y==1:
+	print(x * t)
+if y==2:
+	print(x + t)
+if y==3:
+	print(x / t)
+if y==4:
+	print(x - t)
+if y>4:
+	print('Ошибка')
